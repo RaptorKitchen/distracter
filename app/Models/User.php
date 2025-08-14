0b98208b2934d6e30810fd6134fa55aaed0c04fc
RaptorKitchen <andrewramonlerma@gmail.com>
2025-08-14 12:32:55 -0400

    use storage accessor instead of local path for profile image

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
 
 class User extends Authenticatable
 {
@@ -71,4 +72,10 @@ class User extends Authenticatable
         return $this->hasMany(Reaction::class);
     }
 
+    public function getProfileImageUrlAttribute(): string
+    {
+        return $this->profile_image_filename
+            ? Storage::disk('public')->url($this->profile_image_filename)
+            : asset('images/distracter-logo-only.png'); // fallback
+    }
 }
